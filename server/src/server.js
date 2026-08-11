2cf5d262d9a224faea7ba1cc96b9386b797e803b
vikranth666 <vikranthbolleddula@gmail.com>
2026-08-11 13:28:07 +0530

    initial commit

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+const app = require("./app");
+const connectDB = require("./config/db")
+
+const PORT = process.env.PORT || 5050;
+
+const startServer = async()=>{
+    await connectDB()
+}
+
+app.listen(PORT, () => {
+  console.log(`ClinicFlow API running on port ${PORT}`);
+});
+
+startServer()
