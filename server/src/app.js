387cd17d09586cf8187bd121a645d0c173109473
vikranth666 <vikranthbolleddula@gmail.com>
2026-08-12 13:13:30 +0530

    Add role based authorization

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const cors = require("cors");
 
+const {protect} = require("./middleware/authMiddleware");
+const {authorize} = require("./middleware/roleMiddleware")
+
 const authRoutes = require("./routes/authRoutes")
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+
+app.get("/api/admin-test",protect, authorize("ADMIN"),(req, res)=>{
+    res.status(200).json({
+      success:true,
+      message:"welcome Admin",
+      user: req.user,
+    })
+})
 
 app.get("/api/health", (req, res) => {
   res.status(200).json({
